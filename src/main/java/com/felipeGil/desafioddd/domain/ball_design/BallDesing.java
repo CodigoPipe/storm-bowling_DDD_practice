d1f55082844a0e8eca7fc1cbd6a6dcc0285ab532
felipe <pipega2000@hotmail.com>
2022-08-21 00:27:29 -0500

    BallDesing class in balldesing package finished

--- src/main/java/com/felipeGil/desafioddd/domain/ball_design/BallDesing.java
+++ src/main/java/com/felipeGil/desafioddd/domain/ball_design/BallDesing.java
@@ -95,7 +95,89 @@ public class BallDesing extends AggregateEvent<BallDesingId> {
         appendChange(new SurfaceChanged(coverDesingId, surface));
     }
 
+    public void decreaseBallWeight(BallWeight ballWeight){
+        Objects.requireNonNull(ballWeight);
+        appendChange(new BallWeightDecreased(ballWeight));
+    }
 
+    public void decreaseCoreSize(CoreDesingId coreDesingId, CoreSize coreSize){
+        Objects.requireNonNull(coreDesingId);
+        Objects.requireNonNull(coreSize);
+        appendChange(new CoreSizeDecreased(coreDesingId, coreSize));
+    }
+
+    public void decreaseDensity(ColorDesingId colorDesingId, Density density){
+        Objects.requireNonNull(colorDesingId);
+        Objects.requireNonNull(density);
+        appendChange(new DensityDecreased(colorDesingId, density));
+    }
+
+    public void decreaseResponseTime(CoverDesingId coverDesingId, ResponseTime responseTime){
+        Objects.requireNonNull(coverDesingId);
+        Objects.requireNonNull(responseTime);
+        appendChange(new ResponseTimeDecreased(coverDesingId, responseTime));
+    }
+
+    public void fixStartDateOfBallDesing(StartDate startDate){
+        Objects.requireNonNull(startDate);
+        appendChange(new StartDateOfBallDesingFixed(startDate));
+    }
+
+    public void updateEndDateOfBallDesing(EndDate endDate){
+        Objects.requireNonNull(endDate);
+        appendChange(new EndDateOfBallDesingUpdated(endDate));
+    }
+
+    public void increaseBallWeight(BallWeight ballWeight){
+        Objects.requireNonNull(ballWeight);
+        appendChange(new BallWeightIncreased(ballWeight));
+    }
+
+    public void increaseCoreSize(CoreDesingId coreDesingId, CoreSize coreSize){
+        Objects.requireNonNull(coreDesingId);
+        Objects.requireNonNull(coreSize);
+        appendChange(new CoreSizeIncreased(coreDesingId, coreSize));
+    }
+
+    public void increaseDensity(ColorDesingId colorDesingId, Density density){
+        Objects.requireNonNull(colorDesingId);
+        Objects.requireNonNull(density);
+        appendChange(new DensityIncreased(colorDesingId, density));
+    }
+
+    public void increaseResponseTime(CoverDesingId coverDesingId, ResponseTime responseTime){
+        Objects.requireNonNull(coverDesingId);
+        Objects.requireNonNull(responseTime);
+        appendChange(new ResponseTimeIncreased(coverDesingId, responseTime));
+    }
+
+    public BallName ballName(){
+        return ballName;
+    }
+
+    public BallWeight ballWeight(){
+        return ballWeight;
+    }
+
+    public StartDate startDate(){
+        return startDate;
+    }
+
+    public EndDate endDate(){
+        return endDate;
+    }
+
+    public ColorDesing colorDesing(){
+        return colorDesing;
+    }
+
+    public CoreDesing coreDesing(){
+        return coreDesing;
+    }
+
+    public CoverDesing coverDesing(){
+        return coverDesing;
+    }
 
 
 }
